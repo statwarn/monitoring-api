ad9978656cecefbcfddb3ac9a85da530918dd97a
Romain Baumier <baumier.romain@gmail.com>
2014-12-11 14:46:24 +0100

    routes => POST /metrics

---
+++ api/routes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function(app, InfoModel, esClient, amqp, PrettyError) {
+
+  function sendError(req, res, err) {
+    res.json(err.code || 500, PrettyError.ErrorToJSON(err) || {});
+  }
+
+  InfoModel.setDependencies(esClient, amqp);
+
+  function AuthMiddleware(req, res, next) {
+    var access_token = req.params.access_token;
+    next();
+  }
+
+  app.use('/api', AuthMiddleware);
+
+  app.post('/api/v1/infos', function(req, res) {
+    var timestamp = req.query.timestamp;
+    var server_id = req.query.server_id;
+
+    var info = new InfoModel().fromJSON(server_id, timestamp, req.body);
+
+    if (info instanceof PrettyError) {
+      return sendError(req, res, info);
+    }
+
+    info.create(function(err, info) {
+      if (err) {
+        return sendError(req, res, err);
+      }
+      res.status(200).json(info);
+    });
+  });
+}
