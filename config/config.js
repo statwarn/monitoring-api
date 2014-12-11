52c5b1008f49a2401c9ac0904c13c2c78d745255
Romain Baumier <baumier.romain@gmail.com>
2014-12-11 14:43:21 +0100

    config (amp + elastic search)

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  elasticsearch: {
+    host: 'localhost:9200',
+    log: 'trace'
+  },
+
+  amqp:{
+    login: 'guest',
+    password: 'guest',
+    host: 'localhost',
+    port: 5672
+  }
+}
