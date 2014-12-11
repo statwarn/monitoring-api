d839d844368c477219124ecbdc1278c323d56139
Romain Baumier <baumier.romain@gmail.com>
2014-12-11 14:48:55 +0100

    example of JSON on POST /metrics

---
+++ es_post_example.js
@@ -0,0 +1,55 @@
+{
+  "info": {
+    "metrics": {
+      "uptime_in_seconds": 1525900,
+      "connected_clients": 1,
+      "client_longest_output_list": 0,
+      "client_biggest_input_buf": 0,
+      "blocked_clients": 0,
+      "used_memory": 1079416,
+      "used_memory_rss": 1045760,
+      "used_memory_peak": 1096608,
+      "used_memory_lua": 33792,
+      "mem_fragmentation_ratio": 0.97,
+      "rdb_changes_since_last_save": 0,
+      "rdb_last_save_time": 1415957886,
+      "rdb_last_bgsave_time_sec": -1,
+      "rdb_current_bgsave_time_sec": -1,
+      "aof_rewrite_in_progress": 0,
+      "aof_rewrite_scheduled": 0,
+      "aof_last_rewrite_time_sec": -1,
+      "aof_current_rewrite_time_sec": -1,
+      "total_connections_received": 74,
+      "total_commands_processed": 10135,
+      "instantaneous_ops_per_sec": 0,
+      "rejected_connections": 0,
+      "expired_keys": 0,
+      "evicted_keys": 0,
+      "keyspace_hits": 0,
+      "keyspace_misses": 0,
+      "pubsub_channels": 0,
+      "pubsub_patterns": 0,
+      "latest_fork_usec": 0,
+      "connected_slaves": 0,
+      "used_cpu_sys": 480.31,
+      "used_cpu_user": 234.64,
+      "commands": {
+        "info": {
+          "calls": 10136,
+          "usec": 583086,
+          "usec_per_call": 57.53
+        }
+      },
+      "databases": {
+        "0": {
+          "keys": 50295571,
+          "expires": 0
+        },
+        "1": {
+          "keys": 50295572,
+          "expires": 22
+        }
+      }
+    }
+  }
+}
