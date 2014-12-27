3e819bde57b9f84c03e29d2fffcb2eed6cfc0eb8
FG Ribreau <github@fgribreau.com>
2014-12-27 11:15:09 +0100

    refactor(augment): augment -> augmentReqAndRes

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,11 @@
 'use strict';
-var augment = require('./middlewares/augment');
 
 module.exports = function (models, logger, fOnError) {
+  var augmentReqAndRes = require('./middlewares/augmentReqAndRes');
   var controllers = require('./controllers')(models);
 
   return function (app) {
-    app.use(augment(fOnError));
+    app.use(augmentReqAndRes(fOnError));
 
     app.post('/api/v1/metrics', controllers.metrics.post);
     app.get('/api/v1/metrics', controllers.metrics.get);
