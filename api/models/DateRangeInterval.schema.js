e9fad3fb1a5d475e888da00c7c8e9f797a7668a1
FG Ribreau <github@fgribreau.com>
2014-12-27 12:58:46 +0100

    feat(DateRangeInterval): schema, test and impl

---
+++ api/models/DateRangeInterval.schema.js
@@ -0,0 +1,29 @@
+module.exports = {
+  req: {
+    'title': 'fromReq Schema',
+    'type': 'object',
+
+    'properties': {
+      'interval': {
+        'title': 'Metric interval',
+        'description': 'Interval',
+        'type': 'string',
+        'format': 'enum',
+        'values': ['year', 'quarter', 'month', 'week', 'day', 'hour', 'minute', 'second']
+      },
+      'start_date': {
+        'title': 'date range start date',
+        'description': '',
+        'type': 'string',
+        'format': 'date-time'
+      },
+      'end_date': {
+        'title': 'date range start date',
+        'description': '',
+        'type': 'string',
+        'format': 'date-time'
+      }
+    },
+    'required': ['interval', 'start_date', 'end_date']
+  }
+};
---
+++ api/models/DateRangeInterval.test.js
@@ -0,0 +1,44 @@
+'use strict';
+require('../../bootstrap.test');
+
+var DateRangeInterval = require('./DateRangeInterval');
+
+describe('DateRangeInterval', function () {
+  describe('DateRangeInterval.fromReq', function () {
+    it('should return an error if the query is not defined', function (done) {
+      t.isPrettyError(DateRangeInterval.fromReq(), 400);
+      done();
+    });
+
+    it('should return an error if the interval is not defined', function (done) {
+      t.ok(DateRangeInterval.fromReq({
+        query: {}
+      }) instanceof PrettyError, 'should be a pretty error');
+      // @todo check error.details
+      done();
+    });
+
+    it('should return an error if the interval is defined but invalid', function (done) {
+      t.ok(DateRangeInterval.fromReq({
+        query: {
+          interval: 'plop'
+        }
+      }) instanceof PrettyError, 'should be a pretty error');
+      // @todo check error.details
+      done();
+    });
+
+    it('should return an interval if the interval is valid', function (done) {
+      var dateRangeInterval = DateRangeInterval.fromReq({
+        query: {
+          interval: 'hour',
+          start_date: new Date().toISOString(),
+          end_date: new Date().toISOString(),
+        }
+      });
+
+      t.ok(dateRangeInterval instanceof DateRangeInterval, 'should be an instance of interval is valid');
+      done();
+    });
+  });
+});
