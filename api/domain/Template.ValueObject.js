2295302faf3efee6dc748d97cecd6bc10a9fdf7e
FG Ribreau <github@fgribreau.com>
2014-12-28 17:30:10 +0100

    fix(template): update template generation

--- api/domain/Template.ValueObject.js
+++ api/domain/Template.ValueObject.js
@@ -2,10 +2,13 @@
 /**
  * Template value
  */
-var TEMPLATE_BASE = require('../../template/monitoring.template'); // exceptional
 
-module.exports = function (es, amqp, config) {
+module.exports = function (es, config, template) {
+  assert(_.isString(config.elasticsearch.index.name_prefix));
+  assert(_.isString(config.elasticsearch.index.document.type));
 
+  assert(_.isNumber(config.elasticsearch.index.settings.number_of_shards));
+  assert(_.isNumber(config.elasticsearch.index.settings.number_of_replicas));
 
   /**
    * [Template description]
@@ -18,18 +21,12 @@ module.exports = function (es, amqp, config) {
   }
 
   Template.setupTemplate = function (f) {
-    var USER_DEFINED_TEMPLATE = require('../../template/monitoring.template.redsmin');;
+    var TEMPLATE = _.cloneDeep(template.base);
+    var USER_DEFINED_TEMPLATE = _.cloneDeep(template.defined);
+
     // Setup template
-    assert(_.isString(config.elasticsearch.index.name_prefix));
-    assert(_.isString(config.elasticsearch.index.document.type));
-
-    assert(_.isNumber(config.elasticsearch.index.settings.number_of_shards));
-    assert(_.isNumber(config.elasticsearch.index.settings.number_of_replicas));
-
     var INDEX_NAME_PREFIX = config.elasticsearch.index.name_prefix;
     var INDEX_DOCUMENT_TYPE = config.elasticsearch.index.document.type;
-
-    var TEMPLATE = _.cloneDeep(TEMPLATE_BASE);
 
     // Change template name
     TEMPLATE.template = TEMPLATE.template.replace('{{INDEX_NAME_PREFIX}}', INDEX_NAME_PREFIX);
@@ -46,25 +43,24 @@ module.exports = function (es, amqp, config) {
     TEMPLATE.mappings = newMapping;
 
     // Add user defined mapping
-    mapping.dynamic_templates = USER_DEFINED_TEMPLATE.dynamic_templates || [];
+    mapping.dynamic_templates = mapping.dynamic_templates.concat(USER_DEFINED_TEMPLATE.dynamic_templates || []);
     mapping.properties.data.properties = USER_DEFINED_TEMPLATE.data_mapping || {};
     mapping.properties.metadata.properties = USER_DEFINED_TEMPLATE.metadata_mapping || {};
 
-
-    console.log(JSON.stringify(TEMPLATE, null, 2));
-
     // http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-templates.html#delete
-    es.deleteTemplate({
-        id: TEMPLATE.name
+    es.indices.deleteTemplate({
+        name: TEMPLATE.name
       },
       function (err, body, code) {
+
         if (err && code !== 404) {
           return f(err);
         }
 
         // Create an index template that will automatically be applied to new indices created.
-        es.putTemplate({
-          id: TEMPLATE.name,
+        // https://github.com/elasticsearch/elasticsearch-js/blob/3df3b09d3abf140fe8446b4e19d0014cc8545117/src/lib/apis/1_3.js#L3251
+        es.indices.putTemplate({
+          name: TEMPLATE.name,
           body: TEMPLATE
         }, function (err, body) {
           f(err, body);
