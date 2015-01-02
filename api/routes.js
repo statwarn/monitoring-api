48ef001ba0611c922de12b5b44be2bb2d4d7eef0
FG Ribreau <github@fgribreau.com>
2015-01-02 20:38:46 +0100

    fix(api): error should be displayed using express

--- api/routes.js
+++ api/routes.js
@@ -7,9 +7,17 @@ module.exports = function (logger, es, amqp, fOnError, domain) {
   return function (app) {
     app.use(augmentReqAndRes(fOnError));
 
+
     app.post('/api/v1/measurements', controllers.measurements.post);
     app.get('/api/v1/measurements', controllers.measurements.get);
 
     app.all('/internal/templates/setup', controllers.templates.setup);
+
+    app.use(function (err, req, res, next) {
+      var publicError = new PrettyError(500, 'Internal Server Error', err);
+      // clean stack because it does not say much
+      publicError.stack = null;
+      res.error(publicError);
+    });
   };
 };
