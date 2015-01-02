9abd4f06a38c90bd459b28ccd13fae7fead00010
FG Ribreau <github@fgribreau.com>
2015-01-02 18:47:54 +0100

    fix(Measurement): cleaner api output

--- api/domain/Measurement.Repository.js
+++ api/domain/Measurement.Repository.js
@@ -57,12 +57,29 @@ module.exports = function (es, amqp, config, DateRangeInterval) {
       assert(_.isArray(fields));
       assert(dateRangeInterval instanceof DateRangeInterval);
 
+      var fields_aggs = fields.reduce(function (obj, field, i) {
+        // obj, is an object of:
+        //
+        // 'used_memory': {
+        //   avg: {
+        //     field: 'used_memory'
+        //   }
+        // }
+        //
+        // with "used_memory" == field
+
+        obj[field] = {};
+        obj[field]['avg'] = {
+          field: field
+        };
+        return obj;
+      }, {});
+
       es.search({
         indices: ids.map(makeIndexFromId),
         type: INDEX_DOCUMENT_TYPE,
         fields: fields,
-        // search_type: 'count',
-
+        search_type: 'count',
         body: {
           // size=0 to not show search hits because we only want to see the aggregation results in the response.
           size: 0,
@@ -77,29 +94,56 @@ module.exports = function (es, amqp, config, DateRangeInterval) {
           },
           aggs: {
             volume: {
+              // http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-aggregations-bucket-datehistogram-aggregation.html
               date_histogram: {
                 field: 'timestamp',
                 min_doc_count: 0,
-                interval: dateRangeInterval.interval
-              },
-              aggs: {
-                'used_memory': {
-                  avg: {
-                    field: 'used_memory'
-                  }
+                interval: dateRangeInterval.interval,
+                extended_bounds: {
+                  min: dateRangeInterval.start_ts,
+                  max: dateRangeInterval.end_ts
                 }
-              }
+              },
+              aggs: fields_aggs
             }
           }
         }
       }, function (err, res) {
-        console.log(err, res);
-
         if (err) {
-          return f(new PrettyError('plop', 'plop', err));
+          return f(new PrettyError(500, 'Could not retrieve measurement, try again.', err));
         }
 
-        return f(null, res);
+        //  buckets is an array of
+        //  [{
+        //   key_as_string: "2013-12-29T15:54:00.000Z",
+        //   key: 1388332440000,
+        //   doc_count: 1,
+        //   {{fields[0]}}: { <-- first field name
+        //     value: 3626992
+        //   },
+        //   ... <-- and so on for other fields
+        // },
+        // ...]
+        //
+        // We want to extract :
+        // - `key`, and rename it to `timestamp`
+        // - each fields along with their values
+
+        var data = res.aggregations.volume.buckets.reduce(function (data, bucket) {
+          var point = {
+            timestamp: bucket.key
+          };
+
+          // add fields
+          fields.forEach(function (fieldName) {
+            point[fieldName] = bucket[fieldName].value;
+          });
+
+          data.push(point);
+          return data;
+        }, []);
+
+        return f(null, data, res.took);
       });
     }
   };
