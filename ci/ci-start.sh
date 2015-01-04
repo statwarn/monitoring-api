02d7c0f9fd65464da0c4933a6a99eeeb8782d69d
FG Ribreau <github@fgribreau.com>
2015-01-04 12:30:09 +0100

    chore(ci): improved ci

--- ci/ci-start.sh
+++ ci/ci-start.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env zsh
+export NODE_ENV=preprod
 DIR=${0%/*}
 cd $DIR/..
-set -e
-# Any subsequent commands which fail will cause the shell script to exit immediately
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.21.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install
 nvm use
 npm install
 npm install
-export NODE_ENV=preprod
+set -e
+# Any subsequent commands which fail will cause the shell script to exit immediately
 setopt extended_glob;
+# Allow extended globbing (see fellow)
 `npm bin`/mocha -t 10000 -R spec **/*.test.js~node_modules/*;
 npm install -g check-build
+check-build
