20b6dd53c27c73ea9655abeefd6af19bd02ce615
FG Ribreau <github@fgribreau.com>
2015-01-04 12:09:52 +0100

    chore(package): add check-build to ci process ^^

--- ci/ci-start.sh
+++ ci/ci-start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 DIR=${0%/*}
 cd $DIR/..
+set -e
+# Any subsequent commands which fail will cause the shell script to exit immediately
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.21.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install
@@ -10,3 +12,4 @@ npm install
 export NODE_ENV=preprod
 setopt extended_glob;
 `npm bin`/mocha -t 10000 -R spec **/*.test.js~node_modules/*;
+npm install -g check-build
